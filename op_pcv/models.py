1187039a6e0e0ec09cd53576758e8b93834caee0
verganis <stefano.vergani.it@gmail.com>
2013-04-10 17:10:35 +0200

    Model added __unicode__ for Parlamentare

--- op_pcv/models.py
+++ op_pcv/models.py
@@ -13,49 +13,54 @@ class UltimoAggiornamento(models.Model):
 
 
 class Parlamentare(models.Model):
+    ADESIONE_SELECT = (
+        ('0', 'Aderisce'),
+        ('1', 'Non risponde'),
+        ('2', 'Non aderisce'),
+    )
+
+    RAMO_PARLAMENTO_SELECT = (
+        ('0', 'Camera'),
+        ('1', 'Senato'),
+    )
+
+    GRUPPO_SELECT = (
+        ('0','Popolo della Libertà'),
+        ('1','Partito Democratico'),
+        ('2','Movimento 5 stelle'),
+        ('3','Scelta civica per l\'Italia'),
+        ('4','Sinistra ecologia e libertà'),
+        ('5','Lega Nord e autonomie'),
+        ('6','Fratelli d\'Italia - Centrodestra Nazionale'),
+        ('7','Gruppo per le autonomie-Psi'),
+        ('8','Gruppo Misto'),
+        ('9','Grandi autonomie e libertà'),
+    )
+
     def __unicode__(self):
-        return self.nome
+        str = self.nome+" "+self.cognome
+
+        if self.ramo_parlamento:
+            str+=" - "+self.RAMO_PARLAMENTO_SELECT[int(self.ramo_parlamento)][1]
+        if self.gruppo_parlamentare:
+            str+=" - "+self.GRUPPO_SELECT[int(self.gruppo_parlamentare)][1]
+        if self.adesione:
+            str+=" - "+self.ADESIONE_SELECT[int(self.adesione)][1]
+        return str
 
     class Meta:
         verbose_name_plural = u'Parlamentari'
 
-    ADESIONE_SELECT = (
-
-        (1, 'Aderisce'),
-        (2, 'Non risponde'),
-        (3, 'Non aderisce'),
-    )
-
-    RAMO_PARLAMENTO_SELECT=(
-        (1, 'Parlamento'),
-        (2, 'Senato'),
-    )
-
-    GRUPPO_SELECT=(
-
-        (1,'Popolo della Libertà'),
-        (2,'Partito Democratico'),
-        (3,'Movimento 5 stelle'),
-        (4,'Scelta civica per l\'Italia'),
-        (5,'Sinistra ecologia e libertà'),
-        (6, 'Lega Nord e autonomie'),
-        (7,'Fratelli d\'Italia - Centrodestra Nazionale'),
-        (8, 'Gruppo per le autonomie-Psi'),
-        (9,'Gruppo Misto'),
-        (10,'Grandi autonomie e libertà'),
-    )
-
     nome = models.CharField(max_length=50)
     cognome = models.CharField(max_length=50)
     account_twitter = models.CharField(max_length=20, blank=True)
-    gruppo_parlamentare = models.IntegerField(choices=GRUPPO_SELECT, blank=True, default=None)
+    gruppo_parlamentare = models.CharField(max_length=3,choices=GRUPPO_SELECT, blank=True, default=None)
     risposta_twitter = models.BooleanField(default=False)
     account_mail = models.EmailField(max_length=100, blank=True)
-
     lettura_mail = models.BooleanField(default=False)
     risposta_mail = models.BooleanField(default=False)
-    adesione = models.IntegerField(choices=ADESIONE_SELECT, blank=True, default=None)
-    ramo_parlamento = models.IntegerField(choices=RAMO_PARLAMENTO_SELECT, blank=True, default=None)
+    adesione = models.CharField(max_length=3,choices=ADESIONE_SELECT, blank=True, default=None)
+    ramo_parlamento = models.CharField(max_length=3,choices=RAMO_PARLAMENTO_SELECT, blank=True, default=None)
     attivo = models.BooleanField(default=True)
     gruppo_ristretto = models.BooleanField(default=False)
     data_adesione = models.DateField(blank=True, null=True)
