5f12b6473c7671f8dd4da66e5d62a8ac4c3e0c43
verganis <stefano.vergani.it@gmail.com>
2013-04-15 19:10:22 +0200

    Graph labels: first draft

--- op_pcv/static/js/graph_utils.js
+++ op_pcv/static/js/graph_utils.js
@@ -15,12 +15,14 @@ function draw_arc(div_id, data){
 
     var svg = d3.select("#"+div_id).
         append("svg").
+        data([data]).
         attr("width", width).
         attr("height",height).
         append("g").
         attr("transform", "translate(" + radius*1.5 + "," + radius*1.5 +")");
 
-    var pie = d3.layout.pie().sort(null).startAngle(-90*grad).endAngle(90*grad);
+    var pie = d3.layout.pie().
+        value(function(d) { return d.value; }).startAngle(-90*grad).endAngle(90*grad);
 
     svg.selectAll("path").
         data(pie(data)).
@@ -28,8 +30,26 @@ function draw_arc(div_id, data){
         append("path").
         attr("d",arc).
         attr("fill",function(d, i) { return color(i); }).
+        attr("transform", function(d) {                    //set the label's origin to the center of the arc
+            //we have to make sure to set these before calling arc.centroid
+            d.innerRadius = 0;
+            d.outerRadius = 100;
+            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+        }).
+        attr("text-anchor", "middle")                          //center the text on it's origin
+        .text(function(d, i) { return data[i].label; }).
         transition().
         duration(500).
         attrTween("d", sweep);
 
-}
+//    svg.selectAll("path").append("svg:text")                                     //add a label to each slice
+//        .attr("transform", function(d) {                    //set the label's origin to the center of the arc
+//            //we have to make sure to set these before calling arc.centroid
+//            d.innerRadius = 0;
+//            d.outerRadius = 100;
+//            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+//        })
+//        .attr("text-anchor", "middle")                          //center the text on it's origin
+//        .text(function(d, i) { return data[i].label; });        //get the label from our original data array
+//
+//}
