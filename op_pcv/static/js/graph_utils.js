da16609dcc4d5846ad22a711bb6bfabc8213f3b5
verganis <stefano.vergani.it@gmail.com>
2013-04-30 12:12:28 +0200

    Header ok, graph colors, graph labels ok

--- op_pcv/static/js/graph_utils.js
+++ op_pcv/static/js/graph_utils.js
@@ -20,10 +20,11 @@ function draw_arc(div_id, data, label){
         attr("width", width).
         attr("height",height).
         append("g").
-        attr("transform", "translate(" + radius*1.5 + "," + (radius*1.5+100) +")");
+        attr("transform", "translate(" + radius*1.8 + "," + (radius*1.5+100) +")");
 
     var pie = d3.layout.
         pie().
+        sort(null).
         value(function(d) { return d.value; }).
         startAngle(-90*grad).endAngle(90*grad);
 
@@ -32,7 +33,14 @@ function draw_arc(div_id, data, label){
         enter().
         append("path").
         attr("d",arc).
-        attr("fill",function(d, i) { return color(i); }).
+        attr("fill",function(d, i) {
+            if(data[i].label=="ADERISCONO")
+                return "#60b887";
+            else
+                return "#ff7e79";
+        }).
+        attr("stroke", "white").
+        attr("stroke-width", "4px").
         transition().
         duration(500).
         attrTween("d", sweep);
@@ -40,23 +48,39 @@ function draw_arc(div_id, data, label){
     //stampa il n. di senatori/deputati totale
     svg.append("svg:text")
         .attr("text-anchor", "middle")
-        .text(label[0].value + " " + label[0].label);
-
-    var fontsize=14;
-    var x_offset = -(width/3.6 );
-    var y_offset = height -70;
+        .attr("class","bold_label")
+        .attr("transform", "translate(0,"+offset_ntot+")")
+        .text(label[0].value);
+    //stampa  la label associata ai totali
+    svg.append("svg:text")
+        .attr("transform", "translate(0,"+offset_labeltot+")")
+        .attr("text-anchor", "middle")
+        .text( label[0].label);
 
     //stampa il n. di aderenti/ non aderenti
     svg.append("svg:text")
-        .attr("transform", "translate("+x_offset+",-"+y_offset+")")
+        .attr("transform", "translate("+offset_x_n+",-"+offset_y_n+")")
         .attr("text-anchor", "middle")
-        .text(data[0].value + " " + data[0].label);
+        .attr("class","bold_label")
+        .text(data[0].value);
+
+    svg.append("svg:text")
+        .attr("transform", "translate("+offset_x_n+",-"+offset_y_label+")")
+        .attr("text-anchor", "middle")
+        .text(data[0].label);
 
 
     svg.append("svg:text")
-        .attr("transform", "translate("+ (-x_offset)+",-"+y_offset+")")
+        .attr("transform", "translate("+ (-offset_x_n)+",-"+offset_y_n+")")
         .attr("text-anchor", "middle")
-        .text(data[1].value + " " + data[1].label);
+        .attr("class","bold_label")
+        .text(data[1].value);
+
+    svg.append("svg:text")
+        .attr("transform", "translate("+(-offset_x_n)+",-"+offset_y_label+")")
+        .attr("text-anchor", "middle")
+        .text(data[0].label);
+
 
 
 }
--- op_pcv/static/js/variables.js
+++ op_pcv/static/js/variables.js
@@ -1,9 +1,19 @@
 //pie chart
 var grad=Math.PI/180;
-var width = 300;
-var height = 250;
+var width = 350;
+var height = 260;
 var radius = 100;
 var arc_width = 50;
+
+
+var offset_x_n = -(width/3.6 );
+var offset_y_n = height -70;
+var offset_y_label = offset_y_n-20;
+
+var offset_ntot= -25;
+var offset_labeltot= -3;
+
+
 var arc = d3.svg.arc().innerRadius(radius-arc_width).outerRadius(radius + arc_width);
 
 
