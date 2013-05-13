f3717ed6645af41a2cd51c9da6373dc5d969eff3
Guglielmo <guglielmo.celata@gmail.com>
2013-05-13 16:57:42 +0200

    urls contain lavorincorso pointer

--- op_pcv/urls.py
+++ op_pcv/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,12 +8,14 @@ from op_pcv.views import PcvHome
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', PcvHome.as_view(), name="home"),
-    # url(r'^op_pcv/', include('op_pcv.foo.urls')),
-
+    url(r'^admin', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # work in progress url
+    #url(r'^.*$', TemplateView.as_view(template_name='lavorincorso.html')),
+
+    url(r'^$', PcvHome.as_view(), name="home"),
+
+
 )
