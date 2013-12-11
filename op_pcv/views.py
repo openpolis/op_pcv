5cb49a48cbcc4d0ea1a883ac8f2ae20eaec476c1
verganis <stefano.vergani.it@gmail.com>
2013-12-11 18:15:15 +0100

    Removed partiti sigla from views

--- op_pcv/views.py
+++ op_pcv/views.py
@@ -3,6 +3,7 @@ import datetime
 import socket
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Count
 from django.views.generic import TemplateView
 from op_pcv.models import Parlamentare,GruppoParlamentare, UltimoAggiornamento, Entry
 import feedparser
@@ -87,36 +88,31 @@ class PcvHome(TemplateView):
 
         context['col_camera']=[]
 
-        # sets fixed order for groups in the col chart
-        ordine_gruppi_camera=["Pd","M5S","FI-Pdl","Sel","NCD","Sc","Misto","Lega","PI","Fdi"]
-        ordine_gruppi_senato=["Pd","FI-Pdl","M5S","NCD","Lega","Misto","Aut-Psi","PI","Gal","Sc"]
+        # sets order for groups in the col chart
 
-        for sigla in ordine_gruppi_camera:
-            try:
-                g = GruppoParlamentare.objects.get(sigla=sigla)
-            except ObjectDoesNotExist:
-                pass
-            else:
-                mydict={}
-                mydict["sigla"]=g.sigla
-                mydict["aderenti_tot"]=g.get_n_aderenti(0)
-                mydict["non_aderenti_tot"]=g.get_n_non_aderenti(0)+g.get_n_silenti(0)
-                context['col_camera'].append(mydict)
+        ordine_gruppi_camera = GruppoParlamentare.objects.\
+            filter(parlamentare__ramo_parlamento='0',parlamentare__in_carica=True).\
+            annotate(n=Count("parlamentare")).order_by('-n')
+        ordine_gruppi_senato = GruppoParlamentare.objects. \
+            filter(parlamentare__ramo_parlamento='1',parlamentare__in_carica=True). \
+            annotate(n=Count("parlamentare")).order_by('-n')
+
+        for gruppo_camera in ordine_gruppi_camera:
+            gruppo_c={}
+            gruppo_c["sigla"]=gruppo_camera.sigla
+            gruppo_c["aderenti_tot"]=gruppo_camera.get_n_aderenti(0)
+            gruppo_c["non_aderenti_tot"]=gruppo_camera.get_n_non_aderenti(0)+gruppo_camera.get_n_silenti(0)
+            context['col_camera'].append(gruppo_c)
 
 
         context['col_senato']=[]
 
-        for sigla in ordine_gruppi_senato:
-            try:
-                g = GruppoParlamentare.objects.get(sigla=sigla)
-            except ObjectDoesNotExist:
-                pass
-            else:
-                mydict={}
-                mydict["sigla"]=g.sigla
-                mydict["aderenti_tot"]=g.get_n_aderenti(1)
-                mydict["non_aderenti_tot"]=g.get_n_non_aderenti(1)+g.get_n_silenti(1)
-                context['col_senato'].append(mydict)
+        for gruppo_senato in ordine_gruppi_senato:
+            gruppo_s={}
+            gruppo_s["sigla"]=gruppo_senato.sigla
+            gruppo_s["aderenti_tot"]=gruppo_senato.get_n_aderenti(1)
+            gruppo_s["non_aderenti_tot"]=gruppo_senato.get_n_non_aderenti(1)+gruppo_senato.get_n_silenti(1)
+            context['col_senato'].append(gruppo_s)
 
         context['gruppi_didascalia']=GruppoParlamentare.get_gruppi()
 
